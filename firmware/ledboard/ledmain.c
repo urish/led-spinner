d755a6f4e4146d89f10bdef8d4616601ef536f8e
Uri Shaked <uri@salsa4fun.co.il>
2005-11-01 21:08:03 +0000

    Added preliminary code for I2C (TWI) support. The current code is only a basis for experimenting with the I2C bus, and is not tested yet.

--- firmware/ledboard/ledmain.c
+++ firmware/ledboard/ledmain.c
@@ -10,6 +10,9 @@
 #include <avr/signal.h> 	 // here
 #include "text.h"
 
+// The address of the device on the I2C bus
+#define I2C_DEVICE_ADDRESS	(0x11)		
+
 #define ARRAY_ENTRIES(array) (sizeof(array)/sizeof((array)[0]))
 #define BV(value) (1 << (value))
 
@@ -58,6 +61,7 @@ void beep (unsigned char cycles, unsigned char pitch) {
 // Main program
 //
 int main(void) {
+	int i = 0;
 
 	DDRA = 0xff;
 
@@ -66,18 +70,59 @@ int main(void) {
 	TCCR1B	= counter_resolution;
 	TCNT1	= ~counter_cycle;
 	TIMSK	= BV(TOIE1);
-	sei();
 	
 	/* Setup INT 1 */
 	DDRD	&= ~BV(PD3);
 	PORTD	|= BV(PD3);
 	MCUCR	= (MCUCR & 0xf0) | BV(ISC11);
 	GICR	|= BV(INT1);
+	
+	/* Initialize the I2C / TWI bus in Slave Mode */
+	TWCR	= BV(TWEA) | BV(TWEN);
+	TWAR	= (I2C_DEVICE_ADDRESS << 1);
+	
+	/* Enable interrupts */
+	sei();
 
 	/* Play the first row */
 	PORTA = g_bitmap[g_bitmap_pointer];
 
-	/* Enter sleep mode */
+	/* Startup beep (to be removed) */
 	beep(255, 50);
-	for (;;);
+	
+	/* I2C Bus Polling */
+	for (;;) {
+		if (!(TWCR & BV(TWINT))) {
+			continue;
+		}
+		
+		// We were addressed by the I2C bus; Handle this.
+		switch (TWSR & 0xF8) {
+		case 0x60:	/* Transmission start in Slave-Receive mode */
+			/* Do Nothing */
+			break;
+		
+		case 0x80:	/* Acknowledged data byte received. */
+			/* Copy the received data all over the bitmap */
+			for (i = 0; i < ARRAY_ENTRIES(g_bitmap); i++) {
+				g_bitmap[i] = TWDR;
+			}
+			break;
+			
+		case 0xA8:	/* Transmission start in Slave-Transmit mode */
+			break;
+		
+		case 0xB8:	/* Acknowledged data byte sent */
+			break;
+			
+		case 0xC0:	/* Non-acknowledged data byte sent */
+			break;
+			
+		case 0xC8:	/* Last data byte was sent */
+			break;
+		}
+		
+		// Release the TWI Bus.
+		TWCR &= ~BV(TWINT);
+	}
 }
