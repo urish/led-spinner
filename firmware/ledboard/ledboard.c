8ced9ee1738a8f718c857d7a84d33eaad5134b56
Uri Shaked <uri@salsa4fun.co.il>
2006-02-04 23:02:00 +0000

    Implemented I2C command/answer handling. Currently only Read Version command is implemented.

--- firmware/ledboard/ledboard.c
+++ firmware/ledboard/ledboard.c
@@ -7,6 +7,7 @@
 
 /* Includes */
 #include <pic18fregs.h>
+#include <string.h>
 #include "i2c_slave.h"
 #include "text.h"
 
@@ -20,6 +21,34 @@
 #define TIMER_COUNTER_CYCLE		(49911)
 #define TIMER_PRESCALER_VALUE	(0x5)
 
+const char source_revision[] = "$Rev$";
+
+enum
+{
+	ANSWER_OK,
+	ANSWER_ERROR_CHECKSUM,
+	ANSWER_ERROR_INVALID_COMMAND,
+};
+
+/* Typedefs */
+typedef unsigned char byte;
+typedef unsigned int  word;
+typedef unsigned long dword;
+
+typedef struct
+{
+	byte checksum;
+	byte command_code;
+	byte buf[16];
+} command_packet;
+
+typedef struct
+{
+	byte checksum;
+	byte error_code;
+	byte buf[16];
+} answer_packet;
+
 /* Interrupt handler */
 void interrupt1_handler() interrupt 1
 {
@@ -30,6 +59,56 @@ void interrupt1_handler() interrupt 1
 		TMR0L	= TIMER_COUNTER_CYCLE & 0xff;
 		INTCONbits.TMR0IF	= 0;
 	}
+}
+
+/* Utility function: calculate_checksum() */
+static byte calculate_checksum(data byte * buffer, byte size)
+{
+	byte sum = 0;
+	for (; size; buffer++, size--)
+	{
+		sum += *buffer;
+	}
+	return ~sum + 1;
+}							
+
+void handle_command(void)
+{
+	data command_packet * cmd = (data command_packet *)i2c_incoming.buf;
+	data answer_packet  * answer = (data answer_packet *)i2c_outgoing.buf;
+	
+	memset(answer, 0, sizeof(i2c_outgoing.buf));
+	
+	/* Validate incoming packet */
+	if (calculate_checksum(i2c_incoming.buf, i2c_incoming.size))
+	{
+		answer->error_code = ANSWER_ERROR_CHECKSUM;
+		goto finalize_answer;
+	}
+	
+	answer->error_code = ANSWER_OK;
+	switch (cmd->command_code)
+	{
+	case 0: /* Read version */
+		strcpy(answer->buf, source_revision);
+		break;
+	
+	case 1: /* Read data array */
+		break;
+	
+	case 2: /* Write data array */
+		break;
+	
+	case 3: /* Read speed stats */
+		break;
+	
+	default:
+		answer->error_code = ANSWER_ERROR_INVALID_COMMAND;
+	}
+	
+finalize_answer:
+	i2c_outgoing.size = sizeof(i2c_outgoing.buf);
+	answer->checksum = calculate_checksum((data byte *)answer, i2c_outgoing.size);
 }
 
 // ***********************************************************
@@ -63,5 +142,10 @@ void main(void) {
 	for (;;)
 	{
 		i2c_slave_handler();
+		if (i2c_incoming.size)
+		{
+			handle_command();
+			i2c_incoming.size = 0;
+		}
 	}
 }
