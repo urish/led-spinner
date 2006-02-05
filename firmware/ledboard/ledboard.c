9fd16872df2691163e3c3ac48226071341087e11
Uri Shaked <uri@salsa4fun.co.il>
2006-02-05 20:39:15 +0000

    Ledboard: Added support for INT2 Counters. The new code is still problematic, as I2C code seems to freeze for some reason whenever INT2 occurs.

--- firmware/ledboard/ledboard.c
+++ firmware/ledboard/ledboard.c
@@ -30,6 +30,9 @@ enum
 	ANSWER_ERROR_INVALID_COMMAND,
 };
 
+/* Macros */
+#define ARRAY_ENTRIES(arr) (sizeof(arr)/sizeof((arr)[0]))
+
 /* Typedefs */
 typedef unsigned char byte;
 typedef unsigned int  word;
@@ -48,6 +51,17 @@ typedef struct
 	byte error_code;
 	byte buf[22];
 } answer_packet;
+
+typedef struct
+{
+	dword prev_counters[4];
+	dword current_counter;
+} counter_stat_buffer;
+
+/* Global variables */
+dword	g_int2_times[4];
+byte	g_int2_times_index	 = 0;
+byte	g_int2_counter_upper = 0;
 
 /* Interrupt handler */
 void interrupt1_handler() interrupt 1
@@ -70,12 +84,22 @@ static byte calculate_checksum(data byte * buffer, byte size)
 		sum += *buffer;
 	}
 	return ~sum + 1;
-}							
+}
+
+dword read_int2_counter(void)
+{
+	byte int2_counter_low = TMR1L;
+	return ((dword)g_int2_counter_upper << 16) | 
+			((dword)TMR1H << 8) | 
+			int2_counter_low;
+}
 
 void handle_command(void)
 {
 	data command_packet * cmd = (data command_packet *)i2c_incoming.buf;
 	data answer_packet  * answer = (data answer_packet *)i2c_outgoing.buf;
+	data counter_stat_buffer * counter_stat	= NULL;
+	byte i = 0;
 	
 	memset(answer, 0, sizeof(i2c_outgoing.buf));
 	
@@ -100,6 +124,14 @@ void handle_command(void)
 		break;
 	
 	case 3: /* Read speed stats */
+		counter_stat = (data counter_stat_buffer *)answer->buf;
+		for (i = 0; i < ARRAY_ENTRIES(counter_stat->prev_counters); i++)
+		{
+			counter_stat->prev_counters[i] =
+				g_int2_times[(g_int2_times_index + i) % 
+							 ARRAY_ENTRIES(g_int2_times)];
+		}
+		counter_stat->current_counter = read_int2_counter();
 		break;
 	
 	default:
@@ -114,12 +146,19 @@ finalize_answer:
 // ***********************************************************
 // Main program
 //
-void main(void) {
+void main(void)
+{
 	/* Internal oscilator setup @ 8MHz */
 	OSCCON = 0x7F;
 	
 	/* PortB digital IO */
 	ADCON1 |= 0x0F;
+	
+	/* Pull up port B */
+	INTCON2bits.RBPU = 0;
+	
+	/* Initialization */
+	memset(g_int2_times, 0, sizeof(g_int2_times));
 	
 	/* Configure TMR0 */
 	TMR0H	= TIMER_COUNTER_CYCLE >> 8;
@@ -128,6 +167,22 @@ void main(void) {
 	T0CONbits.TMR0ON	= 1;
 	INTCONbits.TMR0IE	= 1;
 	INTCONbits.TMR0IF	= 0;
+	
+	/* Configure TMR1 */
+	TMR1H	= 0;
+	TMR1L	= 0;
+	T1CONbits.RD16		= 1;	/* 16 bit read/write mode */
+	T1CONbits.T1CKPS0	= 1; 	/* 8 bit prescaler */
+	T1CONbits.T1CKPS1	= 1; 	/* 8 bit prescaler */
+	T1CONbits.TMR1ON	= 1;
+	PIE1bits.TMR1IE		= 1;
+	PIR1bits.TMR1IF		= 0;
+	
+	/* Configure INT2 (on RB2) */
+	TRISBbits.TRISB2	= 1; /* Configure INT2 pin as an input */
+	INTCON2bits.INTEDG2	= 1;
+	INTCON3bits.INT2IE 	= 1;
+	INTCON3bits.INT2IF	= 0;
 	
 	/* Enable interrupts */
 	INTCONbits.GIE = 1;
@@ -146,6 +201,22 @@ void main(void) {
 		{
 			handle_command();
 			i2c_incoming.size = 0;
+		}
+		if (PIR1bits.TMR1IF)
+		{
+			if (g_int2_counter_upper < 0xff)
+			{
+				g_int2_counter_upper++;
+			}
+			PIR1bits.TMR1IF = 0;
+		}
+		if (INTCON3bits.INT2IF)
+		{
+			/* Handle an INT2 event */
+			g_int2_times[g_int2_times_index] = read_int2_counter();
+			g_int2_times_index++;
+			g_int2_times_index %= ARRAY_ENTRIES(g_int2_times);
+			INTCON3bits.INT2IF = 0;
 		}
 	}
 }
